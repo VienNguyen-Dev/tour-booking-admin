027b61a7c9ab52716f53cc9ce6dd25c7ee630620
VienNguyen-Dev <149781283+VienNguyen-Dev@users.noreply.github.com>
2024-10-17 14:47:07 +0700

    fix type error in BookingForm component

--- app/admin/orders/new-order/page.tsx
+++ app/admin/orders/new-order/page.tsx
@@ -1,25 +1,12 @@
 "use client";
-import OrderInfo from "@/components/TourBookingForm";
-import PartnerInfo from "@/components/PartnerInfo";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
-import React, { useEffect, useState } from "react";
+import React from "react";
 import TourBookingForm from "@/components/TourBookingForm";
-import { getAllOrders } from "@/lib/actions/order.actions";
-import { getAllProducts } from "@/lib/actions/product.actions";
 
 const NewOrder = () => {
   const path = usePathname();
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const products = await getAllProducts();
-      if (products) setData(products);
-    };
-    fetchData();
-  }, []);
 
   return (
     <div className="flex flex-col gap-4 p-5">
@@ -32,7 +19,7 @@ const NewOrder = () => {
           <span className=" capitalize">{path.includes("new-order") && "New"}</span>
         </div>
       </h1>
-      <TourBookingForm products={data} />
+      <TourBookingForm />
     </div>
   );
 };
